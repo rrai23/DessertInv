d5d9f4462cdafc12460c17192f6e24a5c2bc933e
Pasquay <22101372@usc.edu.ph>
2025-03-23 20:33:24 +0800

    Was gonna make frontend but my bum ass didnt wanna css so ill learn tailwind sa

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crem de la Creme | Inventory</title>
+</head>
+<body>
+    <p>Type shi</p>
+</body>
+</html>
