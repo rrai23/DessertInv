ea392a6250e5de18dd113134717fc43745ac6f23
Pasquay <22101372@usc.edu.ph>
2025-03-26 11:09:04 +0800

    added tailwind

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crem de la Creme | Inventory</title>
+    <title>Crem de la Crem | Inventory</title>
+    <link rel="stylesheet" href="dist/styles.css">
 </head>
 <body>
-    <p>Type shi</p>
+    <p class="bg-red-500">Type shit</p>
 </body>
 </html>
