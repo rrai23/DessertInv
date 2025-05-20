8abfb22d852bff23e15409bb0490a7bb7f3a89e6
Pasquay <22101372@usc.edu.ph>
2025-05-20 22:27:36 +0800

    s

--- db.php
+++ db.php
@@ -7,7 +7,7 @@ $dbname = "dessert_inventory";
 $table = "crem_de_la_crem";
 $tableName = strtoupper(str_replace("_", " ", $table));
 
-$conn = new mysqli($host, $username, $password, $dbname,3307);
+$conn = new mysqli($host, $username, $password, $dbname,3306);
 if($conn->connect_error){
     die("Connection Failed: " . $conn->connect_error);
 }
